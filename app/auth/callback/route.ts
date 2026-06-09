4df9f1de78d419386fff67cc8737547e33a77a86
carosalazarnz <carosalazarnz@gmail.com>
2026-06-09 23:51:29 +1200

    Create route.ts

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,17 @@
+import { createClient } from '@supabase/supabase-js';
+import { NextResponse } from 'next/server';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const code = url.searchParams.get('code');
+  
+  if (code) {
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+  
+  return NextResponse.redirect(new URL('/', request.url));
+}
